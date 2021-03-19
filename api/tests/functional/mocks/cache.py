b7b7679535c93ef91ad3c808fc624ef98e9ce1db
Dmitry <d.ovcharenko@i-dgtl.ru>
2021-03-19 20:33:01 +0300

    initial commit, FastApi added

---
+++ api/tests/functional/mocks/cache.py
@@ -0,0 +1,17 @@
+from uuid import UUID
+from typing import Optional
+
+from cache.abstract import Cache
+
+
+class MockEmptyCache(Cache):
+
+    async def get(self, obj_id: UUID) -> Optional[str]:
+        return None
+
+    async def put(self, obj_id: UUID, data: str):
+        pass
+
+
+def get_emptycache_mock():
+    return MockEmptyCache()
